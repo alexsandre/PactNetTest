6b4f36b6ee509e5caf0cfdb3599037e57dc31d00
Alexsandre <alexsandrer@gmail.com>
2021-01-14 19:10:30 -0300

    Create controller with method expect in pact

---
+++ PactTest.API/Code/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PactTest.API.Code
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public DateTime RegistrationStart { get; set; }
+        public DateTime RegistrationEnd { get; set; }
+
+    }
+}
---
+++ PactTest.API/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PactTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private IConfiguration configuration;
+        public EventsController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        [Route("{eventId:int}")]
+        public Code.Event Get(int eventId)
+        {
+            
+            return new Code.Event()
+            {
+                Id = 1,
+                Description = "Descrição",
+                Image = "urlimage",
+                Start = DateTime.Now,
+                End = DateTime.Now,
+                RegistrationStart = DateTime.Now,
+                RegistrationEnd = DateTime.Now
+            };
+        }
+    }
+}
